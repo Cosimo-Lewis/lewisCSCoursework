03a4f1e7c67cb088b40eda55eb85f5c09f78605d
Cosimo Lewis <lewis.c@etoncollege.org.uk>
2018-09-26 18:00:14 +0100

    Adding Trello Functionality

--- lewisCSCoursework/LoginViewController.swift
+++ lewisCSCoursework/LoginViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import Firebase
+import HelloTrello
 
 class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        FirebaseApp.description()
+        
+        
+        Trello.init(apiKey: "5978dc70a9a0438ab1b4f4841d400b43", authToken: "9db58891a408c518e3237a5a7ddc223aaca34831a1c483f377141bd67ebb6600")
+        Trello.getAllBoards(<#T##Trello#>)
+//        https://www.oseyeris.com/post/trello-api-wrapper-for-swift
         // Do any additional setup after loading the view.
     }
     
