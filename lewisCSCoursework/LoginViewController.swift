8b187bffcf7e34adb8708e18cfab8e5dd8430b5c
Cosimo Lewis <lewis.c@etoncollege.org.uk>
2018-10-03 18:01:36 +0100

    Testing with Alamofire

--- lewisCSCoursework/LoginViewController.swift
+++ lewisCSCoursework/LoginViewController.swift
@@ -17,10 +17,25 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
         FirebaseApp.description()
 
-        let jeff = Alamo
-        let bob = Trello(apiKey: "5978dc70a9a0438ab1b4f4841d400b43", authToken: "9db58891a408c518e3237a5a7ddc223aaca34831a1c483f377141bd67ebb6600")
+        let todoEndpoint: String = "https://api.trello.com/1/boards/?fields=name,url&key=5978dc70a9a0438ab1b4f4841d400b43&token=5978dc70a9a0438ab1b4f4841d400b43"
+//        url&key=5978dc70a9a0438ab1b4f4841d400b43&token=5978dc70a9a0438ab1b4f4841d400b43"
+        Alamofire.request(todoEndpoint)
+        .responseJSON { response in
+            
+            guard let json = response.result.value as? [String: Any] else {
+                print("didn't get todo object as JSON from API")
+                print("Error: \(response.result.error)")
+                return
+            }
+            
+            guard let todoTitle = json["title"] as? String else {
+                print("Could not get todo title from JSON")
+                return
+            }
+            print("The title is: " + todoTitle)
+        }
+//        let bob = Trello(apiKey: "5978dc70a9a0438ab1b4f4841d400b43", authToken: "5978dc70a9a0438ab1b4f4841d400b43")
 //        bob.init(apiKey: "5978dc70a9a0438ab1b4f4841d400b43", authToken: "9db58891a408c518e3237a5a7ddc223aaca34831a1c483f377141bd67ebb6600")
-        print(bob.getAllBoards(<#(Result<[Board]>) -> Void#>))
 //        https://www.oseyeris.com/post/trello-api-wrapper-for-swift
 //        https://www.raywenderlich.com/35-alamofire-tutorial-getting-started
 //        http://rexstjohn.com/lets-call-rest-apis-with-alamofire-ios-8-and-swift/
